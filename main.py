7567068da5583a0bc3f2ae1bb4810e21e90fb19c
RevCodes <Nathanpauljones1997@gmail.com>
2025-01-17 11:05:39 +0000

    Create a text based game

---
+++ main.py
@@ -0,0 +1,9 @@
+from room import Room
+
+kitchen = Room("kitchen")
+ballroom = Room("ballroom")
+dininghall = Room("dining hall")
+
+kitchen.set_description("A cold dark room with rats inside")
+
+print(kitchen.name)
